1fe6bb6fed307434c09635443e9a5333047eab19
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-10-23 06:40:03 +0530

    Create botstatus_std-deepanshu.py

---
+++ botstatus_std-deepanshu.py
@@ -0,0 +1,53 @@
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+import asyncio
+import datetime
+import pytz
+import os
+
+app = Client(
+    name = "botstatus_std-deepanshu",
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+    session_string = os.environ["SESSION_STRING"]
+)
+TIME_ZONE = os.environ["TIME_ZONE"]
+BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')]
+CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
+MESSAGE_ID = int(os.environ["MESSAGE_ID"])
+BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
+
+async def master_std-deepanshh():
+    async with app:
+            while True:
+                print("Checking...")
+                xxx_std-deepanshu = f"📊 | 𝗟𝗜𝗩𝗘 𝗕𝗢𝗧 𝗦𝗧𝗔𝗧𝗨𝗦"
+                for bot in BOT_LIST:
+                    try:
+                        yyy_pratheek = await app.send_message(bot, "/start")
+                        aaa = yyy_pratheek.id
+                        await asyncio.sleep(10)
+                        zzz_std-deepanshu = app.get_chat_history(bot, limit = 1)
+                        async for ccc in zzz_pratheek:
+                            bbb = ccc.id
+                        if aaa == bbb:
+                            xxx_std-deepanshu += f"\n\n🤖  @{bot}\n        └ **Down** ❌"
+                            for bot_admin_id in BOT_ADMIN_IDS:
+                                try:
+                                    await app.send_message(int(bot_admin_id), f"🚨 **Beep! Beep!! @{bot} is down** ❌")
+                                except Exception:
+                                    pass
+                            await app.read_chat_history(bot)
+                        else:
+                            xxx_std-deepanshu += f"\n\n🤖  @{bot}\n        └ **Alive** ✅"
+                            await app.read_chat_history(bot)
+                    except FloodWait as e:
+                        await asyncio.sleep(e.x)            
+                time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+                last_update = time.strftime(f"%d %b %Y at %I:%M %p")
+                xxx_std-deepanshu += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n**♻️ Refreshes automatically - Powered By STD KING**"
+                await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_pratheek)
+                print(f"Last checked on: {last_update}")                
+                await asyncio.sleep(6300)
+                        
+app.run(master_pratheek())
